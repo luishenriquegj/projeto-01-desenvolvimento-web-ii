7ee9f1fe5f532c9f7eb8c8059cb7ed2b4b0b1483
luis <lhjuca@gmail.com>
2024-11-01 17:10:17 -0300

    updated controllers with comments, added error handling on requests

--- market/src/main/java/br/imd/ufrn/market/controller/ClientController.java
+++ market/src/main/java/br/imd/ufrn/market/controller/ClientController.java
@@ -10,29 +10,82 @@ import java.util.List;
 
 import static java.sql.DriverManager.println;
 
-
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    /**
+     * Handles GET requests to "/clients".
+     * Retrieves and returns a list of all clients from the database.
+     *
+     * @return a list of Client objects.
+     */
     @GetMapping
     public List<Client> listClient(){
         return clientRepository.findAll();
     }
 
+    /**
+     * Handles POST requests to "/clients".
+     * Creates a new client in the database using the provided client data.
+     *
+     * @param client the client data to be saved.
+     * @return the saved Client object.
+     */
     @PostMapping
     public Client createClient(@RequestBody Client client){
         return clientRepository.save(client);
     }
 
+    /**
+     * Handles PUT requests to update an existing client.
+     *
+     * @param client the client data to be updated. The client must have a non-null ID.
+     * @return a ResponseEntity containing the updated Client object if successful,
+     *         or a 404 Not Found status if the client does not exist.
+     * @throws IllegalArgumentException if the client ID is null, indicating that the client cannot be updated without an ID.
+     */
+    @PutMapping
+    public ResponseEntity<Client> updateClient(@RequestBody Client client) {
+
+        // Check if the client ID is null
+        if (null == client.getId()) {
+            throw new IllegalArgumentException("Id cannot be empty when update methods");
+        }
+
+        // Check if the client exists in the repository
+        if (!clientRepository.existsById(client.getId())) {
+            // Return 404 Not Found if the client does not exist
+            return ResponseEntity.notFound().build();
+        }
+
+        // Update the client in the repository
+        Client updatedClient = clientRepository.save(client);
+
+        // Return the updated client with a 200 OK status
+        return ResponseEntity.ok(updatedClient);
+    }
+
+
+
+
+    /**
+     * Handles DELETE requests to "/clients/delete/{id}".
+     * Deletes a client from the database based on the provided client ID.
+     *
+     * @param id the ID of the client to be deleted.
+     * @return a ResponseEntity indicating the result of the operation:
+     *         - 204 No Content if the deletion is successful.
+     *         - 404 Not Found if the client does not exist.
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteClientById(@PathVariable Integer id) {
         println("exists");
         if (clientRepository.existsById(id.longValue())) {
             clientRepository.deleteById(id.longValue());
-
             return ResponseEntity.noContent().build(); // Returns 204 No Content if deletion is successful
         } else {
             return ResponseEntity.notFound().build(); // Returns 404 Not Found if the client does not exist
--- market/src/main/java/br/imd/ufrn/market/controller/ProductController.java
+++ market/src/main/java/br/imd/ufrn/market/controller/ProductController.java
@@ -1,6 +1,5 @@
 package br.imd.ufrn.market.controller;
 
-
 import br.imd.ufrn.market.Entity.Product;
 import br.imd.ufrn.market.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,34 +12,84 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
     @Autowired
     private ProductRepository productRepository;
 
+    /**
+     * Handles POST requests to "/products".
+     * Creates a new product in the database using the provided product data.
+     *
+     * @param product the product data to be saved.
+     * @return the saved Product object.
+     */
     @PostMapping
-    public Product createProduct(@RequestBody  Product product) {
+    public Product createProduct(@RequestBody Product product) {
         return productRepository.save(product);
     }
 
+    /**
+     * Handles PUT requests to update an existing product.
+     *
+     * @param product the product data to be updated. The product must have a non-null ID.
+     * @return the updated Product object.
+     * @throws IllegalArgumentException if the product ID is null, indicating that the product cannot be updated without an ID.
+     */
+    @PutMapping
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        if (product.getId() == null) {
+            throw new IllegalArgumentException("Id cannot be empty when using update methods");
+        }
+
+        if (!productRepository.existsById(product.getId())) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Product updatedProduct = productRepository.save(product);
+
+        return ResponseEntity.ok(updatedProduct);
+    }
+    /**
+     * Handles GET requests to "/products".
+     * Retrieves and returns a list of all products from the database.
+     *
+     * @return a list of Product objects.
+     */
     @GetMapping
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * Handles GET requests to "/products/{id}".
+     * Retrieves and returns a product based on the provided product ID.
+     *
+     * @param id the ID of the product to be retrieved.
+     * @return a ResponseEntity containing the Product object if found,
+     *         or 404 Not Found if the product does not exist.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         Optional<Product> product = productRepository.findById(id);
         return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Handles DELETE requests to "/products/delete/{id}".
+     * Deletes a product from the database based on the provided product ID.
+     *
+     * @param id the ID of the product to be deleted.
+     * @return a ResponseEntity indicating the result of the operation:
+     *         - 204 No Content if the deletion is successful.
+     *         - 404 Not Found if the product does not exist.
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Product> deleteProductById(@PathVariable("id") Long id) {
         if (productRepository.existsById(id)) {
             productRepository.deleteById(id);
-
             return ResponseEntity.noContent().build(); // Returns 204 No Content if deletion is successful
         } else {
-            return ResponseEntity.notFound().build(); // Returns 404 Not Found if the client does not exist
+            return ResponseEntity.notFound().build(); // Returns 404 Not Found if the product does not exist
         }
     }
-
 }
